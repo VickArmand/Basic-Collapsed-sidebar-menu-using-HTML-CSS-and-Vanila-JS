9514e24d4ccbc6de408ef21a3f682daae57bc386
Vicranzer <victormaina1962@gmail.com>
2022-08-15 17:26:15 +0300

    changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Functions for showing and hiding sidebar
 function openNav(){
     document.getElementById('sidebar').style.width="250px";
     document.getElementById('main').style.marginLeft="250px";
@@ -6,3 +7,9 @@ function closeNav(){
     document.getElementById('sidebar').style.width="0";
     document.getElementById('main').style.marginLeft="0";
 }
+// Code for active page indicator in sidebar
+const navLinks=document.getElementById('sidebar').querySelectorAll('li a');
+const activeClass='active';
+navLinks.forEach((link)=>{if(link.href==window.location.href){
+    link.classList.add(activeClass);
+}});
